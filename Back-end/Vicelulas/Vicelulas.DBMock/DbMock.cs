2540da9f0ef8e1085423c6c2bd657a6e03780f88
fernandojunio_silva <fernandojunio_silva@yahoo.com.br>
2018-12-21 13:53:14 -0200

    Mocking

---
+++ Back-end/Vicelulas/Vicelulas.DBMock/DbMock.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Viceluas.Dominio.Dto;
+
+namespace Vicelulas.DBMock
+{
+    class DbMock
+    {
+        public static List<TriboDto> Tribo { get; set; }
+        public static List<SquadDto> Squad { get; set; }
+
+        private static int _NovoId { get; set; }
+        public static int NovoId
+        {
+            get
+            {
+                return _NovoId++;
+            }
+        }
+
+        static DbMock()
+        {
+            _NovoId = 1;
+            Tribo = new List<TriboDto>
+            {
+                new TriboDto { Id = NovoId, Nome = "Formacao", Ativo = true},
+                new TriboDto { Id = NovoId, Nome = "BRINKS", Ativo = true}
+            };
+
+            Squad = new List<SquadDto>
+            {
+                new SquadDto { Id = NovoId, Id_Tribo = Tribo[0].Id , Nome = "NerdBunker", NomeTribo = "Formacao", Ativo = true },
+                new SquadDto { Id = NovoId, Id_Tribo = Tribo[1].Id , Nome = "NerdBunker", NomeTribo = "Formacao", Ativo = true }
+            };
+        }
+    }
+}

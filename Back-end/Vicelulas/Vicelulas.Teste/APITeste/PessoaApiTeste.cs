82873d5684ed50743c95da8246a493c2224ad3da
fernandojunio_silva <fernandojunio_silva@yahoo.com.br>
2018-12-27 16:03:43 -0200

    API TestesUnitarios

--- Back-end/Vicelulas/Vicelulas.Teste/APITeste/PessoaApiTeste.cs
+++ Back-end/Vicelulas/Vicelulas.Teste/APITeste/PessoaApiTeste.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Vicelulas.Api.Controllers;
 using Viceluas.Dominio.Exceções;
+using Vicelulas.Api.Model;
 
 namespace Vicelulas.Teste.APITeste
 {
@@ -71,6 +72,55 @@ namespace Vicelulas.Teste.APITeste
             var actionResult = pessoaController.GetName(nome);
 
             var okObjectResult = (OkObjectResult)actionResult;
+
+            Assert.Equal(valorEsperado, okObjectResult.StatusCode);
+
+        }
+
+        [Fact]
+        public void RetornaStatusOKPost()
+        {
+            SquadController _squadController = new SquadController();
+            int valorEsperado = 201;
+
+            var objSquad = new SquadInput()
+            {
+                Id_tribo = 5,
+                Nome = "Teste26",
+                Ativo = true
+            };
+
+            var actionResult = _squadController.Post(objSquad);
+
+            var okObjectResult = (CreatedAtRouteResult)actionResult;
+
+            Assert.Equal(valorEsperado, okObjectResult.StatusCode);
+
+        }
+
+        [Fact]
+        public void RetornaStatusOKPut()
+        {
+            PessoaController _pessoaController = new PessoaController();
+            int valorEsperado = 202;
+
+            int Id = 1285;
+
+            var objPessoa = new PessoaInput()
+            {
+                Id_squads = 24,
+                Id_login = 8,
+                Id_papel = 7,
+                Id_unidade = 3,
+                Ativo = true,
+                Nome = "Testando",
+                Email = "teste@teste",
+                Permissao = 1,
+            };
+
+            var actionResult = _pessoaController.Put(Id, objPessoa);
+
+            var okObjectResult = (AcceptedResult)actionResult;
 
             Assert.Equal(valorEsperado, okObjectResult.StatusCode);
 

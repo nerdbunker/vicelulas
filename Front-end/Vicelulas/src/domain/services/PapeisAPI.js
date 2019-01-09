5b5574ccc621379f3478a17944d838fc798edf1d
Mateus Brito <mbrito@viceri.com.br>
2019-01-09 13:26:36 -0200

    API de Cargos

--- Front-end/Vicelulas/src/domain/services/PapeisAPI.js
+++ Front-end/Vicelulas/src/domain/services/PapeisAPI.js
@@ -2,7 +2,7 @@ import { http } from '../api/config'
 
 export default {
   // Obtem as Pessoas por: Todas, ID, ID da Squad
-  obterPapel: () => {
+  obterPapel () {
     return http.get('Papel')
   }
 }

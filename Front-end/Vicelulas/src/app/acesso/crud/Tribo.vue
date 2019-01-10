d2f4472369fa5e5ff633e16c8d60e4b7e3bda31b
Mateus Brito <mbrito@viceri.com.br>
2019-01-10 15:48:50 -0200

    CRUD Att

--- Front-end/Vicelulas/src/app/acesso/crud/Tribo.vue
+++ Front-end/Vicelulas/src/app/acesso/crud/Tribo.vue
@@ -22,26 +22,6 @@
                       </v-flex>
                     </v-layout>
                   </v-container>
-                  <template>
-                    <v-container fluid px-0>
-                      <v-checkbox
-                        :label="`Checkbox 1: ${checkbox.toString()}`"
-                        v-model="checkbox"
-                      ></v-checkbox>
-                      <v-radio-group v-model="radioGroup">
-                        <v-radio
-                          v-for="n in 3"
-                          :key="n"
-                          :label="`Radio ${n}`"
-                          :value="n"
-                        ></v-radio>
-                      </v-radio-group>
-                      <v-switch
-                        :label="`Switch 1: ${switch1.toString()}`"
-                        v-model="switch1"
-                      ></v-switch>
-                    </v-container>
-                  </template>
                 </v-card-text>
 
                 <v-card-actions>
@@ -92,6 +72,7 @@ import TribosAPI from '../../../domain/services/TribosAPI'
 
 export default {
   data: () => ({
+    switch1: true,
     dialog: false,
     listaTribos: [],
     headers: [
